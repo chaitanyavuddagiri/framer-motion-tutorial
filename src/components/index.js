168466cf5821c4c48a0c67b35de0cbab33a095ac
Krishna Chaitanya Vuddagiri <vkchaitanya.535@gmial.com>
2022-08-22 15:43:42 +0530

    added loader using framer motion

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Toppings } from './Toppings';
 export { default as Order } from './Order';
 export { default as Header } from './Header';
 export { default as Modal } from './Modal';
+export { default as Loader } from './Loader';
